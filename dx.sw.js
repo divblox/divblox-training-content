5951fb8e3202e75149f5da1adf84fb976ecba082
Dani <dani.simeonov@stratusolve.com>
2020-08-28 14:46:35 +0200

    added new ticket page and crud

--- dx.sw.js
+++ dx.sw.js
@@ -1,4 +1,4 @@
-const PRECACHE = 'precache-v1598615533';
+const PRECACHE = 'precache-v1598615826';
 const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
