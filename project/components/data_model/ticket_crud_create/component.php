0e80abd9cac468145f1e46d63a7593ff93ec38b7
Dani <dani.simeonov@stratusolve.com>
2020-09-02 15:01:29 +0200

    added backend php functions for random unique ID generation

--- project/components/data_model/ticket_crud_create/component.php
+++ project/components/data_model/ticket_crud_create/component.php
@@ -11,6 +11,23 @@ class TicketController extends EntityInstanceComponentController {
     public function __construct($ComponentNameStr = 'Component') {
         parent::__construct($ComponentNameStr);
     }
+
+    public function getNewTicketUniqueId() {
+        // setResult() to either true or false calling the success or failure
+        // function in the front end.
+        $this->setResult(true);
+
+        // setReturnValue() sets the values in an array that will be returned as JSON
+        // when the script completes. It is always a good idea to populate a
+        // "Message" for the front-end
+        $this->setReturnValue("Message", "New unique ID created");
+        // Here we set the value of any additional parameters to return
+        $this->setReturnValue("TicketId", ProjectFunctions::getNewTicketUniqueId());
+
+        // "presentOutput()" returns our array as JSON and stops any
+        // further execution of the current php script
+        $this->presentOutput();
+    }
 }
 $ComponentObj = new TicketController("ticket_crud_create");
 ?>
